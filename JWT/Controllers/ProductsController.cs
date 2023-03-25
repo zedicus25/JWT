967f362838c5d8899952fd44e1307bb49b0e77c3
Olexander Tsyganok <zedicusgd@gmail.com>
2023-03-25 10:50:48 +0200

    refactor products controller

--- JWT/Controllers/ProductsController.cs
+++ JWT/Controllers/ProductsController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Domain.Interfaces.UnitOfWorks;
 using JWT.Roles;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Identity;
+using JWT.Statuses;
 
 namespace JWT.Controllers;
 [ApiController]
@@ -14,188 +16,70 @@ public class ProductsController : ControllerBase
     private readonly IUnitOfWorks _unitOfWorks;
     private readonly ICacheService _cacheService;
     private readonly AzureClient _azureClient;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public ProductsController(IUnitOfWorks unitOfWorks, ICacheService cacheService)
+    public ProductsController(IUnitOfWorks unitOfWorks, ICacheService cacheService, UserManager<IdentityUser> userManager)
     {
         _unitOfWorks = unitOfWorks;
         //_cacheService = cacheService;
         _azureClient = new AzureClient();
+        _userManager = userManager;
     }
 
     [HttpGet]
     [Route("productsList")]
     public async Task<ActionResult<IEnumerable<Product>>> Get()
     {
-        try
-        {
-            List<Product> assets = _cacheService.GetData<List<Product>>("AllAssets");
-            if (assets == null)
-            {
-                var assestSql = _unitOfWorks.ProductRepository.GetAll().Result.ToList();
-                if (assestSql.Count() > 0)
-                {
-                    _cacheService.SetData("AllAssets", assestSql, DateTimeOffset.Now.AddDays(1));
-                    assets = assestSql.ToList();
-                }
-            }
-            return assets.Where(x => x.StatusId != 3).ToList();
-        }
-        catch (Exception ex)
-        {
-        }
-        return _unitOfWorks.ProductRepository.GetAll().Result.Where(x => x.StatusId != 3).ToList();
-    }
-
-    [HttpGet]
-    [Route("productsInPage")]
-    public async Task<ActionResult<IEnumerable<Product>>> Get([FromQuery(Name = "perPage")] int perPage, [FromQuery(Name = "page")] int page)
-    {
-        try
-        {
-            List<Product> assets = _cacheService.GetData<List<Product>>("AllAssets");
-            if (assets != null)
-            {
-                int startIndex = perPage * (page - 1);
-                try
-                {
-                    var res = assets.Where(x => x.StatusId != 3 && x.StatusId != 2).ToList().GetRange(startIndex, perPage);
-                    return res;
-                }
-                catch (ArgumentException ex)
-                {
-                    Product start = assets.Where(x => x.StatusId != 3 && x.StatusId != 2).ElementAtOrDefault(startIndex);
-                    if (start == null)
-                        return new List<Product>();
-                    int endIndex = assets.Where(x => x.StatusId != 3 && x.StatusId != 2).ToList().LastIndexOf(assets.Last()) + 1;
-                    return assets.Where(x => x.StatusId != 3 && x.StatusId != 2).ToList().GetRange(startIndex, endIndex - startIndex);
-                }
-            }
-        }
-        catch (Exception)
-        {
-        }
-
-        List<Product> assets1 = _unitOfWorks.ProductRepository.GetAll().Result.ToList();
-       
-            int startIndex1 = perPage * (page - 1);
-            try
-            {
-                var res = assets1.Where(x => x.StatusId != 3 && x.StatusId != 2).ToList().GetRange(startIndex1, perPage);
-                return res;
-            }
-            catch (ArgumentException ex)
-            {
-                Product start = assets1.Where(x => x.StatusId != 3 && x.StatusId != 2).ElementAtOrDefault(startIndex1);
-                if (start == null)
-                    return new List<Product>();
-                int endIndex = assets1.Where(x => x.StatusId != 3 && x.StatusId != 2).ToList().LastIndexOf(assets1.Last()) + 1;
-                return assets1.Where(x => x.StatusId != 3 && x.StatusId != 2).ToList().GetRange(startIndex1, endIndex - startIndex1);
-            }
+        var data = await TryGetDataFromCache();
+        return data.Where(x => x.StatusId != ((int)ProductStatuses.Deleted) 
+        && x.StatusId != ((int)ProductStatuses.UnVisible)).ToList();
     }
 
     [HttpGet]
     [Route("getProductsInCategory")]
     public async Task<ActionResult<IEnumerable<Product>>> GetByCategoryId([FromQuery(Name = "categoryId")] int categoryId)
     {
-        try
-        {
-            List<Product> smartphones = _cacheService.GetData<List<Product>>("AllAssets");
-            if (smartphones != null)
-            {
-                var smart = smartphones.Where(x => x.CategoryId == categoryId && x.StatusId != 3 && x.StatusId != 2);
-                return smart.ToList();
-            }
-        }
-        catch (Exception)
-        {
-        }
-      
-        var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-        try
-        {
-            if (smart1.Count() > 0)
-                _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-        }
-        catch (Exception)
-        {
-        }
-        return _unitOfWorks.ProductRepository.GetByCategoryId(categoryId).Result;
+        var data = await TryGetDataFromCache();
+        return data.Where(x => x.CategoryId == categoryId && x.StatusId != ((int)ProductStatuses.Deleted)
+        && x.StatusId != ((int)ProductStatuses.UnVisible)).ToList();
     }
-
-
 
     [HttpGet]
     [Route("getProductsInSubCategories")]
-    public ActionResult<IEnumerable<Product>> GetBySubCategories([FromQuery(Name = "categoriesId")] int[] categoriesId,
+    public async Task<ActionResult<IEnumerable<Product>>> GetBySubCategories([FromQuery(Name = "categoriesId")] int[] categoriesId,
         [FromQuery(Name = "categoryId")] int categoryId)
     {
-        try
-        {
-            List<Product> smartphones = _cacheService.GetData<List<Product>>("AllAssets");
-            if (smartphones != null)
-            {
-                var smart = smartphones.Where(x => categoriesId.Any(c => c == x.SubCategoryId) &&
-                x.CategoryId == categoryId);
-                return smart.ToList();
-            }
-        }
-        catch (Exception)
-        {
-
-            throw;
-        }
-        var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-        try
-        {
-            if (smart1.Count() > 0)
-                _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-        }
-        catch (Exception)
-        {
-        }  
-        return _unitOfWorks.ProductRepository.GetProductInSubCategories(categoriesId, categoryId).ToList();
+        var data = await TryGetDataFromCache();
+        return data.Where(x => categoriesId.Any(c => c == x.SubCategoryId)
+        && x.CategoryId == categoryId && x.StatusId != ((int)ProductStatuses.Deleted)
+        && x.StatusId != ((int)ProductStatuses.UnVisible)).ToList();
     }
 
 
     [HttpPost]
     [Authorize(Roles = UserRoles.Manager)]
     [Route("setStatus")]
-    public ActionResult SetStatus([FromQuery(Name = "productId")] int productId, [FromQuery(Name = "statusId")] int statusId)
+    public async Task<ActionResult> SetStatus([FromQuery(Name = "productId")] int productId, [FromQuery(Name = "statusId")] int statusId)
     {
         _unitOfWorks.ProductRepository.SetStatus(productId, statusId);
         if (_unitOfWorks.Commit() > 0)
         {
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-            try
-            {
-                if (smart1.Count() > 0)
-                    _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            }
-            catch (Exception)
-            {
-            }   
+            await SetDataToCache();   
             return Ok();
         }
+
         return NotFound();
     }
 
     [HttpDelete]
     [Authorize(Roles = UserRoles.Manager)]
     [Route("deleteProduct")]
-    public ActionResult DeleteProduct([FromQuery(Name = "productId")] int productId)
+    public async Task<ActionResult> DeleteProduct([FromQuery(Name = "productId")] int productId)
     {
         _unitOfWorks.ProductRepository.SetStatus(productId, 3);
         if (_unitOfWorks.Commit() > 0)
         {
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-            try
-            {
-                if (smart1.Count() > 0)
-                    _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            }
-            catch (Exception)
-            {
-            }        
+            await SetDataToCache();      
             return Ok("Ok");
         }
 
@@ -204,51 +88,23 @@ public class ProductsController : ControllerBase
 
     [HttpGet]
     [Route("getProduct")]
-    public ActionResult<Product> GetProduct([FromQuery(Name = "productId")] int productId)
+    public async Task<ActionResult<Product>> GetProduct([FromQuery(Name = "productId")] int productId)
     {
-        try
-        {
-            List<Product> smartphones = _cacheService.GetData<List<Product>>("AllAssets");
-            if (smartphones != null)
-            {
-                var smart = smartphones.FirstOrDefault(x => x.Id == productId);
-                if (smart != null)
-                    return smart;
-            }
-            var smart1 = _unitOfWorks.ProductRepository.GetProductById(productId);
-            if (smart1 != null)
-                return smart1;
-        }
-        catch (Exception)
-        {
-        }
-        var smart2 = _unitOfWorks.ProductRepository.GetProductById(productId);
-        return smart2;
+        var data = await TryGetDataFromCache();
+        var prod = data.FirstOrDefault(x => x.Id == productId && x.StatusId != ((int)ProductStatuses.Deleted)
+        && x.StatusId != ((int)ProductStatuses.UnVisible));
+        if (prod == null)
+            return NotFound();
+
+        return prod;
     }
 
     [HttpGet]
     [Route("getProductCount")]
-    public ActionResult<int> GetProductsCount()
+    public async Task<ActionResult<int>> GetProductsCount()
     {
-        try
-        {
-            List<Product> allAssets = _cacheService.GetData<List<Product>>("AllAssets");
-            if (allAssets != null)
-            {
-                return allAssets.Count;
-            }
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-            if (smart1.Count() > 0)
-            {
-                _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-                return smart1.Count();
-            }
-        }
-        catch (Exception)
-        {
-        }
-        var smart2 = _unitOfWorks.ProductRepository.GetAll().Result;
-        return smart2.Count();
+        var data = await TryGetDataFromCache();
+        return data.Count();
     }
 
     [HttpPost]
@@ -256,57 +112,38 @@ public class ProductsController : ControllerBase
     [Route("addProduct")]
     public async Task<ActionResult<bool>> AddProduct()
     {
-        AzureResponce responce = await _azureClient.SendFile(Request.Form.Files[0]);
-        if (responce.IsSuccess)
+        AzureResponce photoResponce = await _azureClient.SendFile(Request.Form.Files[0]);
+        AzureResponce assetFileReponce = await _azureClient.SendFile(Request.Form.Files[1]);
+        if (photoResponce.IsSuccess && assetFileReponce.IsSuccess)
         {
-            string url = _azureClient.GetFileUrl(responce.FileName);
-            if (!url.Equals(string.Empty))
+            string photoUrl = _azureClient.GetFileUrl(photoResponce.FileName);
+            string assetUrl = _azureClient.GetFileUrl(assetFileReponce.FileName);
+            if (!photoUrl.Equals(string.Empty) && !assetUrl.Equals(string.Empty))
             {
 
                 var pro = Request.Form["product"];
                 Product product = JsonConvert.DeserializeObject<Product>(pro);
-                product.Photo = url;
+                product.Photo = photoUrl;
+                product.LinkToFile = assetUrl;
                 _unitOfWorks.ProductRepository.Add(product);
             }
         }
 
         if (_unitOfWorks.Commit() > 0)
-        {
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-            try
-            {
-                if (smart1.Count() > 0)
-                    _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            }
-            catch (Exception)
-            { 
-            }
-            return Ok();
-        }
+            await SetDataToCache();
 
         return BadRequest();
-
     }
+
     [HttpPut]
     [Authorize(Roles = UserRoles.Manager)]
     [Route("updateProduct")]
-    public IActionResult UpdateProduct([FromBody] Product smartphone)
+    public async Task<IActionResult> UpdateProduct([FromBody] Product smartphone)
     {
         _unitOfWorks.ProductRepository.Update(smartphone);
         if (_unitOfWorks.Commit() > 0)
         {
-            try
-            {
-                var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-                if (smart1.Count() > 0)
-                    _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
+            await SetDataToCache();
             return Ok();
         }
 
@@ -317,53 +154,20 @@ public class ProductsController : ControllerBase
     [Route("findProduct")]
     public async Task<ActionResult<IEnumerable<Product>>> FindProducts([FromQuery(Name = "productName")] string productName)
     {
-        try
-        {
-            List<Product> allAssets = _cacheService.GetData<List<Product>>("AllAssets");
-            if (allAssets != null)
-            {
-                var smart = allAssets
-                    .Where(x => x.StatusId != 3 && x.StatusId != 2 && x.Name.ToLower().Contains(productName.ToLower()))
-                    .ToList();
-                if (smart != null)
-                    return smart;
-            }
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-            if (smart1.Count() > 0)
-                _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            return smart1.Where(x => x.StatusId != 3 && x.StatusId != 2 && x.Name.ToLower().Contains(productName.ToLower())).ToList();
-        }
-        catch (Exception)
-        {
-        }
-        var smart2 = _unitOfWorks.ProductRepository.GetAll().Result;
-        return smart2.Where(x => x.StatusId != 3 && x.StatusId != 2 && x.Name.ToLower().Contains(productName.ToLower())).ToList();
-
+        var data = await TryGetDataFromCache();
+        return data.Where(x => x.StatusId != ((int)ProductStatuses.Deleted) 
+        && x.StatusId != ((int)ProductStatuses.UnVisible)
+        && x.Name.ToLower().Contains(productName.ToLower())).ToList();
     }
 
     [HttpGet]
     [Route("getPopularProducts")]
     public async Task<ActionResult<IEnumerable<Product>>> GetPopular()
     {
-        try
-        {
-            List<Product> allAssets = _cacheService.GetData<List<Product>>("AllAssets");
-            if (allAssets != null)
-            {
-                var smart = allAssets.Where(x => x.StatusId == 4 && x.StatusId != 3 && x.StatusId != 2).ToList();
-                if (smart != null)
-                    return smart;
-            }
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
-            if (smart1.Count() > 0)
-                _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            return smart1.Where(x => x.StatusId == 4 && x.StatusId != 3 && x.StatusId != 2).ToList();
-        }
-        catch (Exception)
-        {
-        }
-        var smart2 = _unitOfWorks.ProductRepository.GetAll().Result;
-        return smart2.Where(x => x.StatusId == 4 && x.StatusId != 3 && x.StatusId != 2).ToList();
+        var data = await TryGetDataFromCache();
+        return data.Where(x => x.StatusId != ((int)ProductStatuses.Deleted)
+        && x.StatusId != ((int)ProductStatuses.UnVisible) 
+        && x.StatusId == (int)ProductStatuses.Popular).ToList();
     }
 
     [HttpGet]
@@ -371,26 +175,71 @@ public class ProductsController : ControllerBase
     [Route("orderProducts")]
     public async Task<ActionResult<IEnumerable<Product>>> GetOrderLines([FromQuery(Name = "orderId")] int orderId)
     {
-        List<Product> res = new List<Product>();
         var orderLines = _unitOfWorks.OrderLinesRepository.GetAll().Result.Where(x => x.OrderId == orderId);
+        var assets = await TryGetDataFromCache();
+        return assets.Where(x => orderLines.Any(p => p.ProductId == x.Id)).ToList();
+    }
+
+    [HttpGet]
+    [Authorize(Roles = UserRoles.User)]
+    [Route("buyedProducts")]
+    public async Task<ActionResult<IEnumerable<Product>>> GetBuyedProducts([FromQuery(Name = "userLogin")] string userLog) 
+    {
+        var user = await _userManager.FindByNameAsync(userLog);
+
+        if (user == null)
+            return BadRequest("User dont found!");
+
+        List<Order> orders = _unitOfWorks.OrderRepository.GetAll().Result.Where(x => x.UserId == user.Id).ToList();
+        orders.Distinct();
+
+        List<OrderLine> ordersLines = new List<OrderLine>();
+        var allLines = await _unitOfWorks.OrderLinesRepository.GetAll();
+        
+        foreach (var item in orders)
+            ordersLines.AddRange(allLines.Where(x => x.OrderId == item.Id));
+
+        allLines.Distinct();
+
+        var allProducts = await TryGetDataFromCache();
+        HashSet<Product> results = new HashSet<Product>();
+        foreach(var item in ordersLines)
+            results.Add(allProducts.FirstOrDefault(x => x.Id == item.ProductId));
+
+        return results;
+    }
+
+    private async Task SetDataToCache()
+    {
         try
         {
-            List<Product> allAssets = _cacheService.GetData<List<Product>>("AllAssets");
-            if (allAssets != null)
-            {
-                res = allAssets.Where(x => orderLines.Any(p => p.ProductId == x.Id)).ToList();
-                return res;
-            }
-            var smart1 = _unitOfWorks.ProductRepository.GetAll().Result;
+            var smart1 = await _unitOfWorks.ProductRepository.GetAll();
             if (smart1.Count() > 0)
                 _cacheService.SetData("AllAssets", smart1, DateTimeOffset.Now.AddDays(1));
-            return smart1.Where(x => x.StatusId == 4 && x.StatusId != 3 && x.StatusId != 2).ToList();
         }
         catch (Exception)
         {
         }
-        var smart2 = _unitOfWorks.ProductRepository.GetAll().Result;
-        return smart2.Where(x => orderLines.Any(p => p.ProductId == x.Id)).ToList(); ;
     }
+    /// <summary>
+    /// Method <c>TryGetDataFromCache</c> trying get data from cache, if its null, 
+    /// return data form database and write cache.
+    /// </summary>
+    /// <returns>Collection of products</returns>
+    private async Task<IEnumerable<Product>> TryGetDataFromCache()
+    {
+        try
+        {
+            List<Product> assets = _cacheService.GetData<List<Product>>("AllAssets");
+            if (assets != null)
+                return assets;
 
+            await SetDataToCache();
+        }
+        catch (Exception)
+        {
+        }
+
+        return await _unitOfWorks.ProductRepository.GetAll();
+    }
 }
---
+++ JWT/Statuses/ProductStatuses.cs
@@ -0,0 +1,10 @@
+﻿namespace JWT.Statuses
+{
+    public enum ProductStatuses
+    {
+        Visible = 1,
+        UnVisible = 2,
+        Deleted = 3,
+        Popular = 4,
+    }
+}
